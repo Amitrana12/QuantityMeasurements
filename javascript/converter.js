aa87178107a4e754cc3fb1ecf403c27114412425
amit <[amirana14325@gmail.com]>
2021-05-21 08:58:38 +0530

    [Amit Rana]Added::added logic for convert meter to other lenght

--- javascript/converter.js
+++ javascript/converter.js
@@ -69,5 +69,35 @@ const selection = (value,select,number)=>{
 
 };
 
+input.addEventListener("keyup", myResult);
+inputType.addEventListener("change", myResult);
+resultType.addEventListener("change", myResult);
 
 
+inputTypeValue = inputType.value;
+resultTypeValue = resultType.value;
+
+
+function myResult() {
+
+	inputTypeValue = inputType.value;
+	resultTypeValue = resultType.value;
+
+	if (inputTypeValue === "meter" && resultTypeValue === "kilometer") {
+
+		result.value = Number(input.value) * 0.001;
+	} else if (inputTypeValue === "meter" && resultTypeValue === "centimeter") {
+
+		result.value = Number(input.value) * 100;
+
+	} else if (inputTypeValue === "meter" && resultTypeValue === "meter") {
+		result.value = input.value
+	} else if (inputTypeValue === "meter" && resultTypeValue === "millimeter") {
+		result.value = Number(input.value) * 1000;
+	} else if (inputTypeValue === "meter" && resultTypeValue === "micrometer") {
+		result.value = Number(input.value) * 1000000;
+	} else if (inputTypeValue === "meter" && resultTypeValue === "mile") {
+		result.value = Number(input.value) * 0.000621371;
+	}
+
+}
